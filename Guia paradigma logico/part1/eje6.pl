b67ed19f55840b5f211a256203c617cf11b41f89
Junior Alba <juninhoalba@gmail.com>
2025-06-06 15:18:14 -0300

    Subo ejercicios 5 y 6 de la parte 1 de logico

---
+++ Guia paradigma logico/part1/eje6.pl
@@ -0,0 +1,378 @@
+% Sistema solar
+
+% Hechos
+% Estrella: sol
+estrella(sol).
+
+% Planetas: mercurio, venus, tierra, marte, jupiter, saturno, urano, neptuno, pluton
+planeta(mercurio).
+planeta(venus).
+planeta(tierra).
+planeta(marte).
+planeta(jupiter).
+planeta(saturno).
+planeta(urano).
+planeta(neptuno).
+planeta(pluton).
+
+% Lunas de la tierra
+luna(luna).
+
+% Lunas de marte
+luna(deimos).
+luna(phobos).
+
+% Lunas de jupiter
+luna(adrastea).
+luna(aitne).
+luna(amalthea).
+luna(ananke).
+luna(aoede).
+luna(arche).
+luna(autonoe).
+luna(callisto).
+luna(carme).
+luna(callirrhoe).
+luna(carpo).
+luna(chaldene).
+luna(cyllene).
+luna(elara).
+luna(erinome).
+luna(euanthe).
+luna(eukelade).
+luna(euporie).
+luna(europa).
+luna(eurydome).
+luna(ganymede).
+luna(harpalyke).
+luna(hegemone).
+luna(helike).
+luna(hermippe).
+luna(himalia).
+luna(io).
+luna(iocaste).
+luna(isonone).
+luna(kale).
+luna(kallichore).
+luna(kalyke).
+luna(kore).
+luna(leda).
+luna(lysithea).
+luna(magaclite).
+luna(metis).
+luna(mneme).
+luna(orthosie).
+luna(pasiphae).
+luna(pasithee).
+luna(praxidike).
+luna(sinope).
+luna(sponde).
+luna(s2000j11).
+luna(s2003j2).
+luna(s2003j3).
+luna(s2003j4).
+luna(s2003j5).
+luna(s2003j9).
+luna(s2003j10).
+luna(s2003j12).
+luna(s2003j15).
+luna(s2003j16).
+luna(s2003j17).
+luna(s2003j18).
+luna(s2003j19).
+luna(s2003j23).
+luna(taygete).
+luna(thebe).
+luna(thelxinoe).
+luna(themisto).
+luna(thyone).
+
+% Lunas de saturno
+luna(aegir).
+luna(albiorix).
+luna(atlas).
+luna(bebhionn).
+luna(bergelmir).
+luna(bestla).
+luna(calypso).
+luna(daphnis).
+luna(dione).
+luna(enceladus).
+luna(epimetheus).
+luna(erriapo).
+luna(farbauti).
+luna(fenrir).
+luna(fornjot).
+luna(hati).
+luna(helene).
+luna(hyperion).
+luna(hyrokkin).
+luna(iapetus).
+luna(ijiraq).
+luna(janus).
+luna(kari).
+luna(kiviuq).
+luna(loge).
+luna(methone).
+luna(mimas).
+luna(mundilfari).
+luna(narvi).
+luna(paaliaq).
+luna(pallene).
+luna(pan).
+luna(pandora).
+luna(phoebe).
+luna(polydeuces).
+luna(prometheus).
+luna(rhea).
+luna(siarnaq).
+luna(skathi).
+luna(skoll).
+luna(surtur).
+luna(suttungr).
+luna(s2004s07).
+luna(s2004s12).
+luna(s2004s13).
+luna(s2004s17).
+luna(s2006s1).
+luna(s2006s3).
+luna(s2006s4).
+luna(s2007s1).
+luna(s2007s2).
+luna(s2007s3).
+luna(tarvos).
+luna(telesto).
+luna(tethys).
+luna(thrymr).
+luna(titan).
+luna(ymir).
+
+% Lunas de urano
+luna(ariel).
+luna(belinda).
+luna(bianca).
+luna(caliban).
+luna(cordelia).
+luna(cressida).
+luna(cupid).
+luna(desdemona).
+luna(juliet).
+luna(mab).
+luna(margaret).
+luna(miranda).
+luna(oberon).
+luna(ophelia).
+luna(portia).
+luna(prospero).
+luna(puck).
+luna(rosalind).
+luna(setebos).
+luna(stephano).
+luna(s1986u10).
+luna(s2001u2).
+luna(s2001u3).
+luna(titania).
+luna(trinculo).
+luna(umbriel).
+
+% Lunas de neptuno
+luna(despina).
+luna(galatea).
+luna(halimede).
+luna(larissa).
+luna(laomedeia).
+luna(naiad).
+luna(nereid).
+luna(neso).
+luna(proteus).
+luna(psamathe).
+luna(sao).
+luna(thalassa).
+luna(triton).
+
+% Lunas de pluton
+luna(charon).
+luna(nix).
+luna(hydra).
+
+% Tierra
+lunaDe(tierra, luna).
+
+% Marte
+lunaDe(marte, deimos).
+lunaDe(marte, phobos).
+
+% Júpiter
+lunaDe(jupiter, adrastea).
+lunaDe(jupiter, aitne).
+lunaDe(jupiter, amalthea).
+lunaDe(jupiter, ananke).
+lunaDe(jupiter, aoede).
+lunaDe(jupiter, arche).
+lunaDe(jupiter, autonoe).
+lunaDe(jupiter, callisto).
+lunaDe(jupiter, carme).
+lunaDe(jupiter, callirrhoe).
+lunaDe(jupiter, carpo).
+lunaDe(jupiter, chaldene).
+lunaDe(jupiter, cyllene).
+lunaDe(jupiter, elara).
+lunaDe(jupiter, erinome).
+lunaDe(jupiter, euanthe).
+lunaDe(jupiter, eukelade).
+lunaDe(jupiter, euporie).
+lunaDe(jupiter, europa).
+lunaDe(jupiter, eurydome).
+lunaDe(jupiter, ganymede).
+lunaDe(jupiter, harpalyke).
+lunaDe(jupiter, hegemone).
+lunaDe(jupiter, helike).
+lunaDe(jupiter, hermippe).
+lunaDe(jupiter, himalia).
+lunaDe(jupiter, io).
+lunaDe(jupiter, iocaste).
+lunaDe(jupiter, isonone).
+lunaDe(jupiter, kale).
+lunaDe(jupiter, kallichore).
+lunaDe(jupiter, kalyke).
+lunaDe(jupiter, kore).
+lunaDe(jupiter, leda).
+lunaDe(jupiter, lysithea).
+lunaDe(jupiter, magaclite).
+lunaDe(jupiter, metis).
+lunaDe(jupiter, mneme).
+lunaDe(jupiter, orthosie).
+lunaDe(jupiter, pasiphae).
+lunaDe(jupiter, pasithee).
+lunaDe(jupiter, praxidike).
+lunaDe(jupiter, sinope).
+lunaDe(jupiter, sponde).
+lunaDe(jupiter, s2000j11).
+lunaDe(jupiter, s2003j2).
+lunaDe(jupiter, s2003j3).
+lunaDe(jupiter, s2003j4).
+lunaDe(jupiter, s2003j5).
+lunaDe(jupiter, s2003j9).
+lunaDe(jupiter, s2003j10).
+lunaDe(jupiter, s2003j12).
+lunaDe(jupiter, s2003j15).
+lunaDe(jupiter, s2003j16).
+lunaDe(jupiter, s2003j17).
+lunaDe(jupiter, s2003j18).
+lunaDe(jupiter, s2003j19).
+lunaDe(jupiter, s2003j23).
+lunaDe(jupiter, taygete).
+lunaDe(jupiter, thebe).
+lunaDe(jupiter, thelxinoe).
+lunaDe(jupiter, themisto).
+lunaDe(jupiter, thyone).
+
+% Saturno
+lunaDe(saturno, aegir).
+lunaDe(saturno, albiorix).
+lunaDe(saturno, atlas).
+lunaDe(saturno, bebhionn).
+lunaDe(saturno, bergelmir).
+lunaDe(saturno, bestla).
+lunaDe(saturno, calypso).
+lunaDe(saturno, daphnis).
+lunaDe(saturno, dione).
+lunaDe(saturno, enceladus).
+lunaDe(saturno, epimetheus).
+lunaDe(saturno, erriapo).
+lunaDe(saturno, farbauti).
+lunaDe(saturno, fenrir).
+lunaDe(saturno, fornjot).
+lunaDe(saturno, hati).
+lunaDe(saturno, helene).
+lunaDe(saturno, hyperion).
+lunaDe(saturno, hyrokkin).
+lunaDe(saturno, iapetus).
+lunaDe(saturno, ijiraq).
+lunaDe(saturno, janus).
+lunaDe(saturno, kari).
+lunaDe(saturno, kiviuq).
+lunaDe(saturno, loge).
+lunaDe(saturno, methone).
+lunaDe(saturno, mimas).
+lunaDe(saturno, mundilfari).
+lunaDe(saturno, narvi).
+lunaDe(saturno, paaliaq).
+lunaDe(saturno, pallene).
+lunaDe(saturno, pan).
+lunaDe(saturno, pandora).
+lunaDe(saturno, phoebe).
+lunaDe(saturno, polydeuces).
+lunaDe(saturno, prometheus).
+lunaDe(saturno, rhea).
+lunaDe(saturno, siarnaq).
+lunaDe(saturno, skathi).
+lunaDe(saturno, skoll).
+lunaDe(saturno, surtur).
+lunaDe(saturno, suttungr).
+lunaDe(saturno, s2004s07).
+lunaDe(saturno, s2004s12).
+lunaDe(saturno, s2004s13).
+lunaDe(saturno, s2004s17).
+lunaDe(saturno, s2006s1).
+lunaDe(saturno, s2006s3).
+lunaDe(saturno, s2006s4).
+lunaDe(saturno, s2007s1).
+lunaDe(saturno, s2007s2).
+lunaDe(saturno, s2007s3).
+lunaDe(saturno, tarvos).
+lunaDe(saturno, telesto).
+lunaDe(saturno, tethys).
+lunaDe(saturno, thrymr).
+lunaDe(saturno, titan).
+lunaDe(saturno, ymir).
+
+% Urano
+lunaDe(urano, ariel).
+lunaDe(urano, belinda).
+lunaDe(urano, bianca).
+lunaDe(urano, caliban).
+lunaDe(urano, cordelia).
+lunaDe(urano, cressida).
+lunaDe(urano, cupid).
+lunaDe(urano, desdemona).
+lunaDe(urano, juliet).
+lunaDe(urano, mab).
+lunaDe(urano, margaret).
+lunaDe(urano, miranda).
+lunaDe(urano, oberon).
+lunaDe(urano, ophelia).
+lunaDe(urano, portia).
+lunaDe(urano, prospero).
+lunaDe(urano, puck).
+lunaDe(urano, rosalind).
+lunaDe(urano, setebos).
+lunaDe(urano, stephano).
+lunaDe(urano, s1986u10).
+lunaDe(urano, s2001u2).
+lunaDe(urano, s2001u3).
+lunaDe(urano, titania).
+lunaDe(urano, trinculo).
+lunaDe(urano, umbriel).
+
+% Neptuno
+lunaDe(neptuno, despina).
+lunaDe(neptuno, galatea).
+lunaDe(neptuno, halimede).
+lunaDe(neptuno, larissa).
+lunaDe(neptuno, laomedeia).
+lunaDe(neptuno, naiad).
+lunaDe(neptuno, nereid).
+lunaDe(neptuno, neso).
+lunaDe(neptuno, proteus).
+lunaDe(neptuno, psamathe).
+lunaDe(neptuno, sao).
+lunaDe(neptuno, thalassa).
+lunaDe(neptuno, triton).
+
+% Plutón
+lunaDe(pluton, charon).
+lunaDe(pluton, nix).
+lunaDe(pluton, hydra).
+orbita(X,Y):- estrella(X), planeta(Y).
+orbita(X,Y):- lunaDe(X,Y).
