0c362172581c53f5a4ddac6aa6bb926e93d36cbe
Junior Alba <juninhoalba@gmail.com>
2025-06-08 11:06:42 -0300

    Agrego ejercicio 16

--- Guia paradigma funcional/Ejercicios/ejercicio16.rkt
+++ Guia paradigma funcional/Ejercicios/ejercicio16.rkt
@@ -2,3 +2,10 @@
 ;lista y retorne la lista con los mismos valores excepto el que se pasó por parámetro que se agregará
 ;al final.
 ;Ej: (attach-at-end 'prueba '(esto es una)) → (esto es una prueba)
+#lang racket
+(define (attach-at-end elem lista)
+    (if (equal? lista '())
+    (list elem)
+    (cons (car lista) (attach-at-end elem (cdr lista)))))
+
+(attach-at-end 'prueba '(esto es una))
