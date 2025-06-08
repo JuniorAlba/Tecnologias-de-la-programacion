efd27875369895b952add07e7b395e3c778c8bed
Junior Alba <juninhoalba@gmail.com>
2025-06-07 23:05:29 -0300

    Actualizo archivos

--- Guia paradigma funcional/Ejercicios/ejercicio13.rkt
+++ Guia paradigma funcional/Ejercicios/ejercicio13.rkt
@@ -2,12 +2,14 @@
 ;de letras y devuelva una lista agrupando los que son iguales en sublistas.
 ;Ej: (agrupar '(A A B C A B A D C)) → ((AAAA) (BB) (CC) (D))
 #lang racket
+L = []
+L( (A A) B)
 
 (define (inserta elem lista1)
     (letrec ([aux (lambda (elem lista1)
         (cond
         [(empty? lista1) (list (list elem))]
-        [(equal? elem (car (car lista1))) (cons (cons elem (car lista1)) (cdr lista1))] 
+        [(equal? elem (car (car lista1))) (cons (cons elem (car lista1)) (cdr lista1))]
         [else (cons (car lista1) (aux elem (cdr lista1)))]))])
     (aux elem lista1)))
 
@@ -23,3 +25,40 @@
 
 ;(decide (agrupar lista1)
 ;    (letrec ([aux (lambda (lista1))])))
+
+
+
+
+;OTRA FORMA DE HACERLO
+;siempre conviene hacer funciones chicas y utilizar wrapper para utilizar esas funciones
+
+;hay que identificar que funciones hay que hacer
+
+;tomo el primer elemento y recorro toda la lista juntando toda la lista
+
+;agruparE agrupa por elemento
+(define (agruparE Elem Lista)
+    (if (null? Lista)
+        '()
+        (if (equal? Elem (car Lista))
+            (cons Elem (agruparE Elem (cdr Lista)))
+            (agruparE Elem (cdr Lista)))
+    )
+)
+;elimE devuelve una lista sin ese elemento
+(define (elimE Elem Lista)
+    (if (null? Lista)
+        '()
+        (if (equal? Elem (car Lista))
+            (elimE Elem (cdr Lista))
+            (cons (car Lista) (elimE Elem (cdr Lista))))
+    )
+)
+
+(define (agrupar_ANGEL Lista)
+    (if (null? Lista)
+        '()
+        (cons (agruparE (car Lista) Lista) (agrupar (elimE (car Lista) Lista))
+        )
+    )
+)
---
+++ Guia paradigma funcional/ejercicio16.rkt
@@ -0,0 +1,4 @@
+;Defina un procedimiento en Racket llamado attach-at-end que reciba como parámetro un valor y una
+;lista y retorne la lista con los mismos valores excepto el que se pasó por parámetro que se agregará
+;al final.
+;Ej: (attach-at-end 'prueba '(esto es una)) → (esto es una prueba)
